b66a256faa042d2b7fe5438d43d197825913bb39
ravindra_gouda <ravindragouda444@gmail.com>
2024-04-25 14:43:55 +0530

    used modelserializer class

--- app/serializers.py
+++ app/serializers.py
@@ -1,23 +1,11 @@
 from .models import Employee
 from rest_framework import serializers
 
-class EmployeeSerializer(serializers.Serializer):
-    name=serializers.CharField(max_length=30)
-    email=serializers.EmailField()
-    password=serializers.CharField(max_length=30)
-    phone=serializers.IntegerField()
-
-
-    def create(self, validated_data):
-        print("create method called")
-        return Employee.objects.create(**validated_data)
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Employee
+        fields='__all__'
     
-    def update(self, employee, validated_data):
-        newemp=Employee(**validated_data)
-        newemp.id=employee.id;
-        newemp.save()
-        return newemp
-
 class UserSerializer(serializers.Serializer):
     username=serializers.CharField(max_length=30)
     email=serializers.EmailField()
